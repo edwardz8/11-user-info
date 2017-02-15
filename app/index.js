d9b132d28ca76a9d43d9a786f1ad963be1ba8ebd
Zach Edwards <tolklorist@gmail.com>
2017-02-15 17:23:05 -0600

    changes

--- app/index.js
+++ app/index.js
@@ -1,1 +1,9 @@
 import 'whatwg-fetch';
+import data from './data';
+
+//Select elements to be filled and store as variables
+//Set text or attributes of elements to fill elements
+
+debugger;
+const photoPic = document.querySelector('.banner__pic');
+photoPic.src = data.results[0].picture.large;
