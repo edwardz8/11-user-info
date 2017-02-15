f8a07621608b3c6195eec6fab5a0f7356daf205d
Zach Edwards <tolklorist@gmail.com>
2017-02-15 17:51:18 -0600

    first name

--- app/index.js
+++ app/index.js
@@ -4,6 +4,12 @@ import data from './data';
 //Select elements to be filled and store as variables
 //Set text or attributes of elements to fill elements
 
-debugger;
+//debugger;
 const photoPic = document.querySelector('.banner__pic');
 photoPic.src = data.results[0].picture.large;
+
+const profileName = document.querySelector('.profile__name');
+profileName.innerText = data.results[0].name.first;
+
+const profileName = document.querySelector('.profile__name');
+profileName.innerText = data.results[0].name.last;
