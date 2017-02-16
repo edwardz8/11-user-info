8b44a71ed198bc77c9e58c73f3d479518660518f
Zach Edwards <tolklorist@gmail.com>
2017-02-15 19:57:53 -0600

    finished

--- app/index.js
+++ app/index.js
@@ -11,4 +11,11 @@ photoPic.src = data.results[0].picture.large;
 const profileName = document.querySelector('.profile__name');
 profileName.innerText = `${data.results[0].name.first} ${data.results[0].name.last}`;
 
-//const profileName = document.querySelector('.');
+const profileEmail = document.querySelector('.profile__email');
+profileEmail.innerText = data.results[0].email;
+
+const profilePhone = document.querySelector('.profile__phone');
+profilePhone.innerText = data.results[0].phone;
+
+const profileLocal = document.querySelector('.profile__local');
+profileLocal.innerText = `${data.results[0].location.city} ${data.results[0].location.state}`;
