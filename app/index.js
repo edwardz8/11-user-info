7b46f14ab39262ff3b23d7094f46837bf8b539e7
Zach Edwards <tolklorist@gmail.com>
2017-02-15 19:24:38 -0600

    number two

--- app/index.js
+++ app/index.js
@@ -4,12 +4,11 @@ import data from './data';
 //Select elements to be filled and store as variables
 //Set text or attributes of elements to fill elements
 
-//debugger;
+
 const photoPic = document.querySelector('.banner__pic');
 photoPic.src = data.results[0].picture.large;
 
 const profileName = document.querySelector('.profile__name');
-profileName.innerText = data.results[0].name.first;
+profileName.innerText = `${data.results[0].name.first} ${data.results[0].name.last}`;
 
-const profileName = document.querySelector('.profile__name');
-profileName.innerText = data.results[0].name.last;
+//const profileName = document.querySelector('.');
